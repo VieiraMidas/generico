538fc83e8b8361ba3417e052b77fd5cde9e5c494
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-02-19 22:17:14 -0300

    Começou o projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      lista: []
+    };
+  }
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="ui inverted fixed huge menu">
+      <div className="ui container">
+        <a href="index.html" className="item">Generico</a>
+        <button className="ui primary button">criar projeto</button>
+        <div className="ui icon input right menu">
+          <input type="text" placeholder="procurar projeto..." />
+          <i aria-hidden="true" className="search icon"></i>
+        </div>
+      </div>
     </div>
+        
+    <div class="ui card">
+      <div class="image"><img src="/images/avatar/large/matthew.png" /></div>
+        <div class="content">
+          <div class="header">Matthew</div>
+          <div class="meta"><span class="date">Joined in 2015</span></div>
+          <div class="description">Matthew is a musician living in Nashville.</div>
+        </div>
+        <div class="extra content">
+        <a>
+        <i aria-hidden="true" class="user icon"></i>
+      22 Friends
+        </a>
+      </div>
+    </div>
+    </>
   );
 }
-
+}
 export default App;
